d48880835fb19712148ac83e08f938a253caa30d
Patrick de Jong <p.dejong@evs.com>
2022-11-10 16:01:37 +0100

    Correct treesitter now

---
+++ nvim/after/plugin/treesitter.lua
@@ -0,0 +1,10 @@
+
+require'nvim-treesitter.configs'.setup {
+    ensure_installed = "all",
+    sync_install = true,
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    }
+}
+
