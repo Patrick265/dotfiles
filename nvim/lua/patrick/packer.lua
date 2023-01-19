dcfa61b2c39fa5e6b3e6c6acec21045854b59cd3
Patrick de Jong <p.dejong@evs.com>
2023-01-19 10:42:44 +0100

    New lspconfig, fix for telescope to auto detect if it is a git repo

--- nvim/lua/patrick/packer.lua
+++ nvim/lua/patrick/packer.lua
@@ -1,70 +1,80 @@
 vim.cmd [[packadd packer.nvim]]
 
 vim.cmd([[
-	augroup packer_user_config
-	autocmd!
-	autocmd BufWritePost plugins.lua source <afile> | PackerCompile
-	augroup end
+augroup packer_user_config
+autocmd!
+autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+augroup end
 ]])
 
 return require('packer').startup(function(use)
-	-- PACKER
-	use 'wbthomason/packer.nvim'
+    -- PACKER
+    use 'wbthomason/packer.nvim'
 
-	-- Statusbar
-	use {
-		'nvim-lualine/lualine.nvim',
-		requires = { 'kyazdani42/nvim-web-devicons' }
-	}
+    -- Statusbar
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons' }
+    }
 
-	use {
-		'kyazdani42/nvim-tree.lua',
-		requires = {
-			'kyazdani42/nvim-web-devicons', -- optional, for file icons
-		},
-		tag = 'nightly' -- optional, updated every week. (see issue #1193)
-	}
+    use {
+        'kyazdani42/nvim-tree.lua',
+        requires = {
+            'kyazdani42/nvim-web-devicons', -- optional, for file icons
+        },
+        tag = 'nightly' -- optional, updated every week. (see issue #1193)
+    }
     use { 'fgheng/winbar.nvim' }
 
 
-	-- Markdown
-	use {"ellisonleao/glow.nvim"}
+    -- Markdown
+    use {"ellisonleao/glow.nvim"}
 
 
-	-- Telescope dependecies
-	use {
-		'nvim-treesitter/nvim-treesitter',
-		run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-	}
+    -- Telescope dependecies
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+    }
 
-	use {
-		'nvim-telescope/telescope.nvim',
-		tag = '0.1.0',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
+    use {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.0',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
 
-	-- Intellisense
-	use 'neovim/nvim-lspconfig'
-	use 'hrsh7th/cmp-nvim-lsp'
-	use 'hrsh7th/cmp-buffer'
-	use 'hrsh7th/cmp-path'
-	use 'hrsh7th/cmp-cmdline'
-	use 'hrsh7th/nvim-cmp'
-	use {'tzachar/cmp-tabnine', run='./install.sh', requires = 'hrsh7th/nvim-cmp'}
-	use 'saadparwaiz1/cmp_luasnip'
-	use { 'L3MON4D3/LuaSnip', after="nvim-cmp"} 
-	use 'onsails/lspkind.nvim'
-    use 'p00f/nvim-ts-rainbow'
-	-- Themes
-	use 'folke/tokyonight.nvim'
-	use 'vv9k/bogster'
-	use 'EdenEast/nightfox.nvim'
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
+
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-buffer'},
+            {'hrsh7th/cmp-path'},
+            {'saadparwaiz1/cmp_luasnip'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'hrsh7th/cmp-nvim-lua'},
+            {'tzachar/cmp-tabnine'},
+
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},
+            {'rafamadriz/friendly-snippets'},
+        }
+    }
+    -- Themes
+    use 'folke/tokyonight.nvim'
+    use 'vv9k/bogster'
+    use 'EdenEast/nightfox.nvim'
     use 'Yagua/nebulous.nvim'
     use 'bluz71/vim-moonfly-colors'
     use 'sainnhe/sonokai'
     use 'nvim-tree/nvim-web-devicons'
-	-- Utility
-	use 'norcalli/nvim-colorizer.lua'
+    -- Utility
+    use 'norcalli/nvim-colorizer.lua'
     use 'lewis6991/gitsigns.nvim'
 end)
 
