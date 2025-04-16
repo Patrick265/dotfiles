eb2fad97f55ccbb5e74b686dc14e68cdcc5e55c4
Patrick265 <Patrick.dejong@kpnmail.nl>
2025-04-16 13:49:22 +0200

    Added alacritty, dunst, and updated

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
+# Add this script to your wm startup file.
+
+DIR="$HOME/.config/polybar/"
+
 # Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
-polybar-msg cmd quit
-# Otherwise you can use the nuclear option:
-# killall -q polybar
+killall -q polybar
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 
-echo "---" | tee -a /tmp/polybar1.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
 
-echo "Bar launched..."
+for monitor in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    echo "Starting top bar on monitor '$monitor'"
+    MONITOR="$monitor" polybar -q main -c "$DIR"/config.ini &
+done
